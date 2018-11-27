e488fa77b442d040b61ae3ba3e6b886669a344cf
Yukti Chauhan <11548318@xuber.com>
2018-11-27 15:58:52 +0530

    e

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'cal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('yukti');
+    expect(app.title).toEqual('cal');
   });
 
   it('should render title in a h1 tag', () => {
